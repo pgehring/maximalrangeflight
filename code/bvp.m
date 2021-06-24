1699add1c5f5f29c3b8bd1f6c0571d92079b116e
Philipp Gehring <philipp_gehring@yahoo.de>
2021-06-24 18:49:19 +0200

    fixed bvp

--- code/bvp.m
+++ code/bvp.m
@@ -14,10 +14,9 @@ function Z_dot = bvp(t, Z, param)
     % lam_dot(t) = -H_x
     Z_dot([5:8], 1) = -[
         0;
-        (C_D_0 + k * C_L^2)*Z(3)^2*Z(7)-C_L*(3)*Z(8)*((alpha*beta*F*exp(-beta*Z(2)))/(2*m));
-        Z(5)*cos(Z(4))+Z(6)*sin(Z(4))-(C_D_0 + k * C_L^2)*Z(3)^2*Z(7)-C_L*(3)*Z(8)*((alpha*beta*F*exp(-beta*Z(2)))/(2*m))+Z(8)*g*cos(Z(2))/Z(3)^2;
-        -Z(5)*sin(Z(4)) + Z(6)*Z(3)*cos(Z(4)) + Z(7)*g*cos(Z(4)) - Z(8)*sin(Z(4));
+        Z(7)/(2*m)*(C_D_0 + k * C_L^2)*Z(3)^2*(alpha*beta*F*exp(-beta*Z(2))) - Z(8)/(2*m)*C_L*Z(3)*(alpha*beta*F*exp(-beta*Z(2)));
+        Z(5)*cos(Z(4)) + Z(6)*sin(Z(4)) - Z(7)/(m)*(C_D_0 + k * C_L^2)*Z(3)*(alpha*beta*F*exp(-beta*Z(2))) - Z(8)/(2*m)*(C_L*Z(3)*alpha*beta*F*exp(-beta*Z(2))+2*m*g/(Z(3)^2)*cos(Z(2)));
+        -Z(5)*Z(3)*sin(Z(4)) + Z(6)*Z(3)*cos(Z(4)) - Z(7)*g*cos(Z(4)) + Z(8)*g/Z(3)*sin(Z(4));
     ];
-    
 
 end
